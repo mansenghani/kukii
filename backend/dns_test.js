bea9eecaa782f0abb5bdbd735c1255ff00c4e96a
Manav <senganimanav012@gmail.com>
2026-02-27 23:14:31 +0530

    Deleted Unwanted Files

--- backend/dns_test.js
+++
@@ -1,8 +0,0 @@
-const dns = require('dns');
-dns.resolveSrv('_mongodb._tcp.cluster0.zcawqhy.mongodb.net', (err, addresses) => {
-    if (err) {
-        console.error('DNS SRV lookup failed:', err);
-    } else {
-        console.log('DNS SRV lookup success:', addresses);
-    }
-});
--- backend/dns_test2.js
+++
@@ -1,8 +0,0 @@
-const dns = require('dns');
-dns.resolve4('cluster0.zcawqhy.mongodb.net', (err, addresses) => {
-    if (err) {
-        console.error('DNS A lookup failed:', err);
-    } else {
-        console.log('DNS A lookup success:', addresses);
-    }
-});
